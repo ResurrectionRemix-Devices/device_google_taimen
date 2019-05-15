9d105360ff71ecdf6f77e53c7492a66ddd18a04b
Lunarixus <Nathanb644@hotmail.co.uk>
2019-05-15 08:02:31 +0000

    Build Qualcomm Qti and IMS
    Thanks to @edi194

--- device.mk
+++ device.mk
@@ -136,3 +136,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=1000000
 
+#QTIAudio
+PRODUCT_PACKAGES += \
+    QtiAudio
+	
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager.xml \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
+# system props for ims features
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
